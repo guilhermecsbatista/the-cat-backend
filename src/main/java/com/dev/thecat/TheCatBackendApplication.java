1b6e6b8b448353a6a876051c52b7849514fa2589
Guilherme Cesar <guilhermecsbatista@gmail.com>
2020-12-09 16:45:39 -0300

    feat: add initial project

---
+++ src/main/java/com/dev/thecat/TheCatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dev.thecat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/thecat/TheCatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.thecat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

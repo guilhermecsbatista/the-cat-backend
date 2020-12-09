1137dfc1dc4e931f502574c08a4a1f49753d64ea
Guilherme Cesar <guilhermecsbatista@gmail.com>
2020-12-09 17:02:56 -0300

    feat: add config database

--- src/test/java/com/dev/thecat/TheCatBackendApplicationTests.java
+++ src/test/java/com/dev/thecat/TheCatBackendApplicationTests.java
@@ -1,13 +1,16 @@
 package com.dev.thecat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class TheCatBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void main() {
+        TheCatBackendApplication.main(new String[]{"--spring.config.location=classpath:application-test.yml"});
+    }
 
 }

4dd9e865d0c26abd658ce890a90b538e5bc3ec75
Guilherme Cesar <guilhermecsbatista@gmail.com>
2020-12-09 19:43:56 -0300

    feat: add config restTemplate

---
+++ src/main/java/com/dev/thecat/app/provider/api/config/RestTemplateConfig.java
@@ -0,0 +1,27 @@
+package com.dev.thecat.app.provider.api.config;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Value("${application.client.default.timeout}")
+    private Integer restDefaultTimeout;
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpClient httpClient = HttpClients.createDefault();
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+        factory.setConnectTimeout(restDefaultTimeout);
+        factory.setReadTimeout(restDefaultTimeout);
+        restTemplate.setRequestFactory(factory);
+        return restTemplate;
+    }
+}
---
+++ src/test/java/com/dev/thecat/app/provider/api/config/RestTemplateConfigTest.java
@@ -0,0 +1,24 @@
+package com.dev.thecat.app.provider.api.config;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class RestTemplateConfigTest {
+    private RestTemplateConfig restTemplateConfig;
+
+    @Before
+    public void setUp() {
+        this.restTemplateConfig = new RestTemplateConfig();
+        ReflectionTestUtils.setField(this.restTemplateConfig, "restDefaultTimeout", 5000);
+    }
+
+    @Test
+    public void restTemplateTest() {
+        this.restTemplateConfig.restTemplate();
+    }
+
+}

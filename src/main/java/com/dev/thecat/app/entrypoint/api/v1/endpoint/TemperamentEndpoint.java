871c22055b29fded8dc4b1e660482bd18273a424
Guilherme Cesar <guilhermecsbatista@gmail.com>
2020-12-10 08:37:33 -0300

    feat: add endpoint get by id temperament breeds

---
+++ src/main/java/com/dev/thecat/app/entrypoint/api/v1/endpoint/TemperamentEndpoint.java
@@ -0,0 +1,38 @@
+package com.dev.thecat.app.entrypoint.api.v1.endpoint;
+
+import com.dev.thecat.app.entrypoint.api.v1.response.BreedResponse;
+import com.dev.thecat.domain.breed.usecase.BreedGetByTemperamentIdUseCase;
+import com.dev.thecat.domain.breed.usecase.exception.DataNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "v1/temperaments", produces = {"application/json"})
+public class TemperamentEndpoint {
+
+    private final BreedGetByTemperamentIdUseCase breedGetByTemperamentIdUseCase;
+
+    public TemperamentEndpoint(final BreedGetByTemperamentIdUseCase breedGetByTemperamentIdUseCase) {
+        this.breedGetByTemperamentIdUseCase = breedGetByTemperamentIdUseCase;
+    }
+
+    @GetMapping(value = "{id}/breeds")
+    @ResponseStatus(HttpStatus.OK)
+    public List<BreedResponse> getBreedByTemperamentId(@PathVariable("id") @Valid UUID id) {
+        try {
+            return this.breedGetByTemperamentIdUseCase.execute(id).stream().map(BreedResponse::fromDomain).collect(Collectors.toList());
+        } catch (DataNotFoundException ex) {
+            return Collections.emptyList();
+        }
+    }
+}
---
+++ src/test/java/com/dev/thecat/app/entrypoint/api/v1/endpoint/TemperamentEndpointTest.java
@@ -0,0 +1,61 @@
+package com.dev.thecat.app.entrypoint.api.v1.endpoint;
+
+import com.dev.thecat.domain.breed.entity.seed.BreedEntitySeeder;
+import com.dev.thecat.domain.breed.usecase.BreedGetByTemperamentIdUseCase;
+import com.dev.thecat.domain.breed.usecase.exception.DataNotFoundException;
+import lombok.SneakyThrows;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class TemperamentEndpointTest {
+
+    @Mock
+    private BreedGetByTemperamentIdUseCase breedGetByTemperamentIdUseCase;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        final TemperamentEndpoint controller = new TemperamentEndpoint(this.breedGetByTemperamentIdUseCase);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    @SneakyThrows
+    public void getBreedByTemperamentIdTest() {
+        when(breedGetByTemperamentIdUseCase.execute(any())).thenReturn(Collections.singletonList(BreedEntitySeeder.getBreedEntity()));
+        this.mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/v1/temperaments/" + UUID.randomUUID() + "/breeds")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    public void getBreedByTemperamentIdDataNotFoundTest() {
+        when(breedGetByTemperamentIdUseCase.execute(any())).thenThrow(new DataNotFoundException("Breed not found"));
+        this.mockMvc.perform(
+                MockMvcRequestBuilders
+                        .get("/v1/temperaments/" + UUID.randomUUID() + "/breeds")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
